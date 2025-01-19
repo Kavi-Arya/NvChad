03bb9910b87f30dba72972c5b8c07c19661e48cf
Kavi Arya <kaviarya047@tutamail.com>
2025-01-19 10:57:30 +0530

    general updates

---
+++ lua/nvchad/plugins-fuckme/init.lua
@@ -0,0 +1,22 @@
+return {
+  "nvim-lua/plenary.nvim",
+
+  {
+    "nvchad/base46",
+    build = function()
+      require("base46").load_all_highlights()
+    end,
+  },
+
+  {
+    "nvchad/ui",
+    lazy = false,
+    config = function()
+      require "nvchad"
+    end,
+  },
+
+  "nvzone/volt",
+  "nvzone/menu",
+  { "nvzone/minty", cmd = { "Huefy", "Shades" } },
+}
