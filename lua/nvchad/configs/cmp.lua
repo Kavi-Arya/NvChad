f9460d82ab429e20c5b9171430baead2d024dfbb
Kavi Arya <kaviarya047@tutamail.com>
2025-01-28 15:37:03 +0530

    cmp codecomplition additions

--- lua/nvchad/configs/cmp.lua
+++ lua/nvchad/configs/cmp.lua
@@ -60,7 +60,10 @@ local options = {
     { name = "buffer" },
     { name = "nvim_lua" },
     { name = "path" },
-    { name = "codeium" },
+    per_filetype = {
+      codecompanion = { "codecompanion" },
+    },
+    -- { name = "codeium" },
     {
       name = "spell",
       option = {
