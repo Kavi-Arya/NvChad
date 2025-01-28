6ccd54fa68c4f3a739ace7dd5c570a6fccda8c03
Kavi-Arya <110332389+Kavi-Arya@users.noreply.github.com>
2025-01-28 11:44:52 +0530

    Update cmp.lua

--- lua/nvchad/configs/cmp.lua
+++ lua/nvchad/configs/cmp.lua
@@ -18,7 +18,6 @@ local options = {
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
     ["<C-Space>"] = cmp.mapping.complete(),
     ["<C-e>"] = cmp.mapping.close(),
-    ["<A-y>"] = require('minuet').make_cmp_map(),
 
     ["<CR>"] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Insert,
@@ -62,7 +61,6 @@ local options = {
     { name = "nvim_lua" },
     { name = "path" },
     { name = "codeium" },
-    { name = 'minuet' },
     {
       name = "spell",
       option = {
