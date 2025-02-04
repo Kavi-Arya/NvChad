9d63dfd7b1b8248091e6e52e9707fddee3f17e36
Kavi Arya <kaviarya047@tutamail.com>
2025-02-04 13:10:33 +0530

    options.lua updates

--- lua/nvchad/options.lua
+++ lua/nvchad/options.lua
@@ -32,7 +32,7 @@ g.loaded_ruby_provider = 0
 vim.opt.guifont = "Comic Code:h13" -- the font used in graphical neovim applications
 
 vim.opt.backup = false -- creates a backup file
-vim.opt.clipboard = "unnamedplus" -- allows neovim to access the system clipboard
+-- vim.opt.clipboard = "unnamedplus" -- allows neovim to access the system clipboard
 vim.opt.cmdheight = 1 -- more space in the neovim command line for displaying messages
 vim.opt.completeopt = { "menuone", "noselect" } -- mostly just for cmp
 vim.opt.conceallevel = 1 -- so that `` is visible in markdown files
