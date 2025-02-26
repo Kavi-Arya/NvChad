042236f80a054925ffd0cbdba5d8b2241a80ea18
Kavi Arya <kaviarya047@tutamail.com>
2025-02-26 13:22:26 +0530

    cmp tab updates

--- lua/nvchad/configs/cmp.lua
+++ lua/nvchad/configs/cmp.lua
@@ -25,15 +25,15 @@ local options = {
       select = true,
     },
 
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif require("luasnip").expand_or_jumpable() then
-        require("luasnip").expand_or_jump()
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
+    -- ["<Tab>"] = cmp.mapping(function(fallback)
+    --   if cmp.visible() then
+    --     cmp.select_next_item()
+    --   elseif require("luasnip").expand_or_jumpable() then
+    --     require("luasnip").expand_or_jump()
+    --   else
+    --     fallback()
+    --   end
+    -- end, { "i", "s" }),
 
     ["<S-Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
--- lua/nvchad/mappings.lua
+++ lua/nvchad/mappings.lua
@@ -145,7 +145,7 @@ map("n", "<leader>o",
   opts)
 
 -- theprimegen remaps
--- map("n", "<leader>pv", vim.cmd.Ex)
+map("n", "<leader>pv", vim.cmd.Ex)
 
 map("v", "J", ":m '>+1<CR>gv=gv")
 map("v", "K", ":m '<-2<CR>gv=gv")
@@ -170,7 +170,7 @@ map({ "n", "v" }, "<leader>d", [["_d]])
 -- This is going to get me cancelled
 map("i", "<C-c>", "<Esc>")
 
--- map("n", "<leader>f", vim.lsp.buf.format)
+map("n", "<leader>lf", vim.lsp.buf.format)
 
 
 map("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
